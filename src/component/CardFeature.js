4bf4071f70970e7987a4a2316324d245349476ea
AdiriOghenetega <tegaadiri3@yahoo.com>
2023-06-26 17:01:08 +0100

    updated cardfeature.js

--- src/component/CardFeature.js
+++ src/component/CardFeature.js
@@ -25,7 +25,7 @@ const CardFeature = ({ image, name, price, category, loading, id }) => {
             to={`/product/${id}`}
             onClick={() => window.scrollTo({ top: "0", behavior: "smooth" })}
           >
-            <div className="h-28 flex flex-col justify-center items-center">
+            <div className="h-32 flex flex-col justify-center items-center">
               <img src={image} className="h-full w-full rounded" />
             </div>
             <h3 className="font-semibold text-slate-600  capitalize text-lg mt-4 whitespace-nowrap overflow-hidden">
