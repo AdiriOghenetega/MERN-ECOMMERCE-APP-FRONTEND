8c193ce8883d8ac8ea07ae7765157b3e47f5a4de
AdiriOghenetega <tegaadiri3@yahoo.com>
2023-11-17 14:16:34 +0100

    edited intropage.js

--- src/page/intro.js
+++ src/page/intro.js
@@ -47,55 +47,54 @@ const Intro = () => {
         </div>
       )}
       <div className="w-full flex flex-col items-center bg-hero-pattern bg-no-repeat bg-cover">
-      <div className="flex flex-col justify-center items-center max-h-auto mb-8 w-full">
-        <h1 className="text-2xl md:text-4xl text-[rgb(233,142,30)] p-2 drop-shadow-lg text-center my-8">
+        <h1 className="text-2xl md:text-4xl text-[rgb(233,142,30)] p-2 drop-shadow-lg text-center my-4">
           Welcome to Hcue Restaurant
         </h1>
-        <h1 className="md:text-3xl text-2xl text-[rgb(233,142,30)] p-2 drop-shadow-lg text-center my-4 bg-[rgb(255,255,255,.8)] p-4 rounded">
-        How to order
-        </h1>
-        <div className="flex flex-col md:flex-row justify-center md:justify-evenly items-center w-full">
-          <div className="flex flex-col justify-start items-center border-2 p-2 drop-shadow h-[300px] bg-[rgb(255,255,255,.8)] min-w-[80%] md:min-w-0">
-            <div className="flex flex-col justify-center my-10 items-center text-[rgb(233,142,30)] animate-bounce">
-              <MdShareLocation size="100px" />
+        <div className="flex flex-col justify-center items-center h-[450px] md:bg-[right 30rem] w-full backdrop-blur-[1.5px] my-8 md:my-4">
+          <IntroComponent closed={closed} />
+        </div>
+        <hr className="w-[80%]" />
+        <div className="flex flex-col justify-center items-center max-h-auto mb-8 w-full">
+          <h1 className="md:text-3xl text-2xl text-[rgb(233,142,30)] p-2 drop-shadow-lg text-center my-4 bg-[rgb(255,255,255,.8)] p-4 rounded">
+            How to order
+          </h1>
+          <div className="flex flex-col md:flex-row justify-center md:justify-evenly items-center w-full">
+            <div className="flex flex-col justify-start items-center border-2 p-2 drop-shadow h-[300px] bg-[rgb(255,255,255,.8)] min-w-[80%] md:min-w-0">
+              <div className="flex flex-col justify-center my-10 items-center text-[rgb(233,142,30)] animate-bounce">
+                <MdShareLocation size="100px" />
+              </div>
+              <h2 className="text-2xl text-bold text-center text-[rgb(233,142,30)]">
+                Select nearest location
+              </h2>
+              <h3 className=" w-[250px] text-center">
+                Select the restaurant closest to your pick-up/delivery location.
+              </h3>
             </div>
-            <h2 className="text-2xl text-bold text-center text-[rgb(233,142,30)]">
-              Select nearest location
-            </h2>
-            <h3 className=" w-[250px] text-center">
-              Select the restaurant closest to your pick-up/delivery
-              location.
-            </h3>
-          </div>
-          <div className="flex flex-col justify-start items-center border-2 p-2 drop-shadow h-[300px] bg-[rgb(255,255,255,.8)] min-w-[80%] md:min-w-0 mt-4 md:mt-0">
-            <div className="flex flex-col justify-center my-10 items-center text-[rgb(233,142,30)] animate-bounce">
-              <MdFoodBank size="100px" />
+            <div className="flex flex-col justify-start items-center border-2 p-2 drop-shadow h-[300px] bg-[rgb(255,255,255,.8)] min-w-[80%] md:min-w-0 mt-4 md:mt-0">
+              <div className="flex flex-col justify-center my-10 items-center text-[rgb(233,142,30)] animate-bounce">
+                <MdFoodBank size="100px" />
+              </div>
+              <h2 className="text-2xl text-bold text-center text-[rgb(233,142,30)]">
+                Choose your meal
+              </h2>
+              <h3 className=" w-[250px] text-center">
+                Place your order by choosing from the numerous delicacies on our
+                menu.
+              </h3>
             </div>
-            <h2 className="text-2xl text-bold text-center text-[rgb(233,142,30)]">
-              Choose your meal
-            </h2>
-            <h3 className=" w-[250px] text-center">
-              Place your order by choosing from the numerous delicacies on our
-              menu.
-            </h3>
-          </div>
-          <div className="flex flex-col justify-start items-center border-2 p-2 drop-shadow h-[300px] bg-[rgb(255,255,255,.8)] min-w-[80%] md:min-w-0 mt-4 md:mt-0">
-            <div className="flex flex-col justify-center my-10 items-center text-[rgb(233,142,30)] animate-bounce">
-              <IoFastFoodOutline size="100px" />
+            <div className="flex flex-col justify-start items-center border-2 p-2 drop-shadow h-[300px] bg-[rgb(255,255,255,.8)] min-w-[80%] md:min-w-0 mt-4 md:mt-0">
+              <div className="flex flex-col justify-center my-10 items-center text-[rgb(233,142,30)] animate-bounce">
+                <IoFastFoodOutline size="100px" />
+              </div>
+              <h2 className="text-2xl text-bold text-center text-[rgb(233,142,30)]">
+                Enjoy your meal
+              </h2>
+              <h3 className=" w-[250px] text-center">
+                delivered to your doorstep.
+              </h3>
             </div>
-            <h2 className="text-2xl text-bold text-center text-[rgb(233,142,30)]">
-              Enjoy your meal
-            </h2>
-            <h3 className=" w-[250px] text-center">
-              delivered to your doorstep.
-            </h3>
           </div>
         </div>
-      </div>
-      <hr className="w-[80%] m-8" />
-      <div className="flex flex-col justify-center items-center h-[530px] md:bg-[right 30rem] w-full backdrop-blur-[1.5px] mb-8">
-        <IntroComponent closed={closed} />
-      </div>
       </div>
       <hr className="w-[80%] m-8" />
       <div className="h-[500px] w-full flex flex-col justify-center items-center relative">
@@ -106,18 +105,20 @@ const Intro = () => {
             order, and pick up in-store or have it delivered to your doorstep.
           </h3>
           <div className="flex md:w-[330px] w-full justify-between items-center">
-            <a href="https://expo.dev/artifacts/eas/27yr1yq7LhTyyGaRfZTp4Q.apk" target="_blank">
-            <img
-              src={gstore}
-              alt="googlestore"
-              className="h-[45px] md:w-[150px] w-[40%] w-auto drop-shadow-lg"
-            />
-            </a>
-            <img
-              src={istore}
-              alt="applestore"
+            <a
+              href="https://expo.dev/artifacts/eas/27yr1yq7LhTyyGaRfZTp4Q.apk"
+              target="_blank"
               className="h-[45px] md:w-[150px] w-[40%] drop-shadow-lg"
-            />
+            >
+              <img src={gstore} alt="googlestore" className="h-full w-full" />
+            </a>
+            <a
+              href="https://expo.dev/artifacts/eas/27yr1yq7LhTyyGaRfZTp4Q.apk"
+              target="_blank"
+              className="h-[45px] md:w-[150px] w-[40%]  drop-shadow-lg"
+            >
+              <img src={istore} alt="applestore" className="h-full w-full" />
+            </a>
           </div>
         </div>
 
